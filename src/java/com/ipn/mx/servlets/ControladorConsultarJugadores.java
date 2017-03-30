513e0e3f9055e214578d2572d119019c1b513c21
earredondo <edgar.arredondo.basurto@gmail.com>
2017-03-29 22:51:07 -0600

    Uso de json

--- src/java/com/ipn/mx/servlets/ControladorConsultarJugadores.java
+++ src/java/com/ipn/mx/servlets/ControladorConsultarJugadores.java
@@ -14,6 +14,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 
 /**
  *
@@ -36,30 +41,30 @@ public class ControladorConsultarJugadores extends HttpServlet {
         /*
         Consultar de la base de datos los jugadores del representante
         */
+        JsonArrayBuilder arreglo = Json.createArrayBuilder();
+            
         String nombre = "edgar";
         String apellidoPaterno = "arredondo";
         String apellidoMaterno = "basurto";
         String correo = "edgar@edgar.com";
         String foto = "unaFoto.jpg";
+        /*
+        Dentro de un ciclo for recorrer el set resultante y por cada jugador enviar la siguiente
+        cadena
+        */
+        //for(){
+        arreglo = arreglo.add(Json.createObjectBuilder()
+            .add("nombre", nombre)
+            .add("apellidoPaterno", apellidoPaterno)
+            .add("apellidoMaterno", apellidoMaterno)
+            .add("correo", correo)
+            .add("foto", foto));
+        //}
+        JsonArray value = arreglo.build();
         response.setContentType("text/plain;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            /*
-            Dentro de un ciclo for recorrer el set resultante y por cada jugador enviar la siguiente
-            cadena
-            */
-            out.print("<tr>\n" +
-                            "<th scope='row'>1</th>\n" +
-                            "<td>" + nombre + "</td>\n" +
-                            "<td>" + apellidoPaterno + "</td>\n" +
-                            "<td>" + apellidoMaterno + "</td>\n" +
-                            "<td>" + correo + "</td>\n" +
-                            "<td>" + foto + "</td>\n" +
-                            "<td>\n" +
-                            "<i class='fa fa-edit fa-1x editPlayer icono' aria-hidden='true' data-name='" + nombre + ":" + apellidoPaterno + ":" + apellidoMaterno + "'></i>\n" +
-                            "<i class='fa fa-close fa-1x deletePlayer icono' aria-hidden='true' data-name='" + nombre + ":" + apellidoPaterno + ":" + apellidoMaterno + "'></i>\n" +
-                            "</td>\n" +
-                        "</tr>");
+            out.print(value);
         }
     }
 
