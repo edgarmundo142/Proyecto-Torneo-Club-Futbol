90937542b3948216b8259347c53fd97780a1f017
BlackSpartan117 <masterofpuppets_zeromaldad@hotmail.com>
2017-04-01 10:42:08 -0600

    pantalla crear torneo

--- web/js/misFunciones.js
+++ web/js/misFunciones.js
@@ -52,4 +52,203 @@ function antPagina() {
 	x[1].setAttribute("class", "nav-link active");
 }
 
+function Fecha() {
+	this.dia  = "00";
+	this.mes  = "00";
+	this.anio = "0000";
+    
+	this.getDia = function() {
+		return this.dia;
+	}
+	
+	this.getMes = function() {
+       return this.mes;
+    }
+    
+    this.getAnio = function() {
+       return this.anio;
+    } 
+    
+    this.setDia = function( dia ) {
+		var m = parseInt( this.getMes() );
+		var diaValido = false;
+		
+		if( m % 2 == 1 || m == 8 || m == 10 || m == 12 ) {
+			if( parseInt( dia ) > 0 && parseInt( dia ) <= 31 ) {
+				this.dia = dia;
+				diaValido = true;
+			}
+			
+		} else if( m % 2 == 0 || m == 9 || m == 11 ) {
+			if( m == 2 && parseInt( dia ) > 0 && parseInt( dia ) <= 29 ) {
+				this.dia = dia;
+				diaValido = true;
+				
+			} else if( parseInt( dia ) > 0 && parseInt( dia ) <= 30 ) {
+			
+				this.dia = dia;
+				diaValido = true;
+			}
+		}
+		
+		return diaValido;
+	}
+    
+    this.setMes = function( mes ) {
+		if( parseInt( mes ) > 0 && parseInt( mes ) <= 12 ) {
+			this.mes = mes;
+			return true;
+		} else 
+			return false;
+    } 
+    
+    this.setAnio = function( anio ) {
+	    var fecha = new Date();
+	    var anioActual = fecha.getFullYear();
+	    
+		if( parseInt( anio ) >= parseInt( anioActual ) && parseInt( anio ) <= parseInt( anioActual ) + 1 ) {
+			this.anio = anio;
+			
+			return true;
+		} 
+		
+		return false;
+    }
+    
+    this.setFecha = function( fecha ) {
+    	var f = fecha.split("/");
+    	var fechaValida = false;
+    	
+    	if( f.length == 3 ) {
+    		if( this.setMes( f[0] ) )
+	    		if( this.setDia( f[1] ) )
+					if( this.setAnio( f[2] ) )
+						fechaValida = true;
+    	} 
+    	
+    	return fechaValida;
+    }
+    
+    this.setFechaActual = function() {
+		var fecha = new Date();
+		var dia, mes, anio;
+	
+		dia = fecha.getDate();
+		mes = fecha.getMonth() + 1;
+		anio = fecha.getFullYear();
+	
+		if( dia <= 9 ) {
+			dia = "0" + dia;
+		}
+	
+		if( mes <= 9 ) {
+			mes = "0" + mes;
+		}
+	
+		this.setDia( dia );
+		this.setMes( mes );
+		this.setAnio( anio );
+	}
+	
+	this.getFecha = function() {
+		return this.getMes() + "/" + this.getDia() + "/" + this.getAnio();
+	}
+}
+
+
+function fechaTerminoTorneo() {
+	var fecha = document.getElementById("iniTorneo").value;
+	var fechaActual = new Fecha();
+	var fecha1 = new Fecha();
+	
+	fechaActual.setFechaActual();
+	if( !fecha1.setFecha( fecha ) ) {
+		alert("FECHA NO VALIDA\n");
+		return;
+	}
+	
+	if( parseInt( fecha1.getMes() ) >= parseInt( fechaActual.getMes() ) ) {
+		if( parseInt( fecha1.getMes() ) == parseInt( fechaActual.getMes() ) ) {
+			if( parseInt( fecha1.getDia() ) < parseInt( fechaActual.getDia() ) ) {
+				alert("No se puede iniciar un torneo con una fecha pasada\n");
+				document.getElementById("iniTorneo").value = fechaActual.getFecha();
+				
+				return;
+			}
+		}
+	}
+	
+	var x = $.datepicker.iso8601Week( new Date( fechaActual.getAnio(), fechaActual.getMes(), fechaActual.getDia() ) );
+	alert( x + 4 );
+}
+
 window.addEventListener("load", botones_Ant_Sig );
+
+
+$.validate( {
+	form:"#siguiente",
+	lang:"es",
+	modules : 'file',
+	onSuccess: function(){
+		var lobibox = Lobibox.confirm({
+			msg: "Est&aacute;s seguro que quires registrar al jugador ",
+			title: "Confirmaci&oacute;n",
+			buttons: {
+				yes: {
+					'class': 'btn btn-success',
+					text: "Si",
+					closeOnClick: true
+				},
+				cancel: {
+					'class': 'btn btn-danger',
+					text: 'Cancelar',
+					closeOnClick: true
+				}
+			},
+			
+			callback: function(lobibox, type){
+				if(type == 'yes'){
+					$.ajax({
+						method:"post",
+						url:"ControladorRegistrarJugador",
+						data:"nombre=edgar&correo=hola@hola.com&telefono=7731256415",
+						success: function(resp){
+							alert(resp);
+						}
+					});
+					Lobibox.notify("success",{
+						title:"Jugador actualizado",
+						msg:"Se registr&oacute; al jugador correctamente",
+						position:"bottom right",
+						delay:4000,
+						width:400,
+						iconSource:"fontAwesome"
+					});
+				}
+			},
+		});
+	
+		return false;
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
